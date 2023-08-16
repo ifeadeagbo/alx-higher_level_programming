7019afcfdf550e9eb28c36e862eeb0a7bbe6a3a8
ifeadeagbo <isadeagbo04@gmail.com>
2023-08-16 04:48:05 -0700

    function that computes square values

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for y in range(len(matrix)):
+        new_matrix[y] = list(map(lambda x : x ** 2, matrix[y]))
+
+    return (new_matrix)
