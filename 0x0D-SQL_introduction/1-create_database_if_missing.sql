63084fcbfbf376679d9ddd21dffc2e77817fbb56
ifeadeagbo <isadeagbo04@gmail.com>
2023-10-19 14:12:17 -0700

    sql introduction

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Attempt to create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
+
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Attempt to drop the database if it exists
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
+
